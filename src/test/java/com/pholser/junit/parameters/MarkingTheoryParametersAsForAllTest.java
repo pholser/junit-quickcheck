39f4e5bbebecec28098bd372498af9e78ebc5cfa
Paul Holser <pholser@alumni.rice.edu>
2011-03-02 22:17:39 -0600

    Renaming README to README.md -- let's see about this markdown thingamabobber.

--- src/test/java/com/pholser/junit/parameters/MarkingTheoryParametersAsForAllTest.java
+++ src/test/java/com/pholser/junit/parameters/MarkingTheoryParametersAsForAllTest.java
@@ -1,12 +1,11 @@
 package com.pholser.junit.parameters;
 
-import static com.pholser.junit.parameters.Annotations.*;
-
 import org.junit.Test;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
+import static com.pholser.junit.parameters.Annotations.*;
 import static org.junit.Assert.*;
 import static org.junit.experimental.results.PrintableResult.*;
 import static org.junit.experimental.results.ResultMatchers.*;
