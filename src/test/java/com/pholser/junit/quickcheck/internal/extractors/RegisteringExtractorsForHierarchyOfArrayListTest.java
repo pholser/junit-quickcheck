baef51a66e6d578695c5c6f130b7ba4dcd80a545
Paul Holser <pholser@alumni.rice.edu>
2011-11-19 10:25:19 -0600

    more tests proving how an extractor becomes eligible to supply random values for theory parms of types all up the hierarchy of a class.

--- src/test/java/com/pholser/junit/quickcheck/internal/extractors/RegisteringExtractorsForHierarchyOfArrayListTest.java
+++ src/test/java/com/pholser/junit/quickcheck/internal/extractors/RegisteringExtractorsForHierarchyOfArrayListTest.java
@@ -1,7 +1,36 @@
+/*
+ The MIT License
+
+ Copyright (c) 2010-2011 Paul R. Holser, Jr.
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package com.pholser.junit.quickcheck.internal.extractors;
 
+import java.io.Serializable;
+import java.util.AbstractCollection;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
+import java.util.RandomAccess;
 
 import com.pholser.junit.quickcheck.RandomValueExtractor;
 import org.junit.Before;
@@ -38,6 +67,48 @@ public class RegisteringExtractorsForHierarchyOfArrayListTest {
     @Test
     public void list() {
         RandomValueExtractor<?> result = repo.extractorFor(List.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void randomAccess() {
+        RandomValueExtractor<?> result = repo.extractorFor(RandomAccess.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void cloneable() {
+        RandomValueExtractor<?> result = repo.extractorFor(Cloneable.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void serializable() {
+        RandomValueExtractor<?> result = repo.extractorFor(Serializable.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void abstractCollection() {
+        RandomValueExtractor<?> result = repo.extractorFor(AbstractCollection.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void collection() {
+        RandomValueExtractor<?> result = repo.extractorFor(Collection.class);
+
+        assertSingleExtractor(result);
+    }
+
+    @Test
+    public void iterable() {
+        RandomValueExtractor<?> result = repo.extractorFor(Iterable.class);
 
         assertSingleExtractor(result);
     }
