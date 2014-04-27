165efe7fc931765ce1590adbaedaf28d421035ef
Paul Holser <pholser@alumni.rice.edu>
2014-04-26 22:05:35 -0500

    Updating copyright dates

--- core/src/test/java/com/pholser/junit/quickcheck/internal/generator/GenerationContextTest.java
+++ core/src/test/java/com/pholser/junit/quickcheck/internal/generator/GenerationContextTest.java
@@ -1,7 +1,7 @@
 /*
  The MIT License
 
- Copyright (c) 2010-2013 Paul R. Holser, Jr.
+ Copyright (c) 2010-2014 Paul R. Holser, Jr.
 
  Permission is hereby granted, free of charge, to any person obtaining
  a copy of this software and associated documentation files (the
