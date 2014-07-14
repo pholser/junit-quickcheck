4f9d293a6bb2a5b271a0e87af6901531c4ee78f3
Paul Holser <pholser@alumni.rice.edu>
2014-07-14 10:13:32 -0500

    LambdaGenerator now a Generator<T> instead of a Generator<Object>

--- core/src/main/java/com/pholser/junit/quickcheck/internal/generator/LambdaGenerator.java
+++ core/src/main/java/com/pholser/junit/quickcheck/internal/generator/LambdaGenerator.java
@@ -31,12 +31,12 @@ import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 
 import static com.pholser.junit.quickcheck.generator.Lambdas.*;
 
-public class LambdaGenerator<T, U> extends Generator<Object> {
+public class LambdaGenerator<T, U> extends Generator<T> {
     private final Class<T> lambdaType;
     private final Generator<U> returnValueGenerator;
 
     public LambdaGenerator(Class<T> lambdaType, Generator<U> returnValueGenerator) {
-        super(Object.class);
+        super(lambdaType);
 
         this.lambdaType = lambdaType;
         this.returnValueGenerator = returnValueGenerator;
