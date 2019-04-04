277f686634fb1d8d2325fa1335961970e590ad9a
Paul Holser <pholser@alumni.rice.edu>
2019-04-04 11:27:37 -0500

    Upgrade FindBugs Maven plugin
    Correct `ConventionGen` test not to assume file system; read
    URL resources as streams instead.

--- examples/src/test/java/com/pholser/junit/quickcheck/examples/convention/ConventionGenTest.java
+++ examples/src/test/java/com/pholser/junit/quickcheck/examples/convention/ConventionGenTest.java
@@ -6,39 +6,46 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Enumeration;
 
 import static org.junit.Assert.*;
 
 @RunWith(JUnitQuickcheck.class)
 public class ConventionGenTest {
-
     @Property
-    public void random_convention_objects_are_generated(Convention convention1, Convention convention2) {
-        assertNotNull(convention1);
-        assertNotNull(convention2);
-        assertNotSame(convention1, convention2);
+    public void random_convention_objects_are_generated(
+        Convention first,
+        Convention second) {
+
+        assertNotNull(first);
+        assertNotNull(second);
+        assertNotSame(first, second);
     }
 
     @Test
-    public void generator_class_is_not_defined_in_services_file() throws IOException, URISyntaxException {
-        String servicesFileName = "META-INF/services/com.pholser.junit.quickcheck.generator.Generator";
-        Enumeration<URL> serviceFiles = Thread.currentThread().getContextClassLoader().getResources(servicesFileName);
+    public void generator_class_is_not_defined_in_services_file()
+        throws Exception {
+
+        String servicesFileName =
+            "META-INF/services/com.pholser.junit.quickcheck.generator.Generator";
+        Enumeration<URL> serviceFiles =
+            Thread.currentThread().getContextClassLoader().getResources(servicesFileName);
         if (serviceFiles == null || !serviceFiles.hasMoreElements()) {
             return;
         }
+
         while (serviceFiles.hasMoreElements()) {
             URL serviceFile = serviceFiles.nextElement();
-            try (BufferedReader reader = Files.newBufferedReader(Paths.get(serviceFile.toURI()))) {
-                assertTrue(reader.lines().noneMatch(ConventionGen.class.getName()::equals));
+
+            try (BufferedReader reader =
+                new BufferedReader(new InputStreamReader(serviceFile.openStream()))) {
+
+                assertTrue(
+                    reader.lines().noneMatch(
+                        ConventionGen.class.getName()::equals));
             }
         }
     }
-
-
 }
