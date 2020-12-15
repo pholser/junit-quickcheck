c9118ddfe01650190ab7d5160ebee4bb647b49a0
Luke Yi <lukeyi@pku.edu.cn>
2020-12-15 06:56:59 -0800

    Update PropertiesWithExplicitGeneratorsTest.java (#292)
    Clear the static List WithExplicitGenerator.values at the end of the test explicitGeneratorTakesPrecedence

--- core/src/test/java/com/pholser/junit/quickcheck/PropertiesWithExplicitGeneratorsTest.java
+++ core/src/test/java/com/pholser/junit/quickcheck/PropertiesWithExplicitGeneratorsTest.java
@@ -279,6 +279,7 @@ public class PropertiesWithExplicitGeneratorsTest {
     @Test public void explicitGeneratorTakesPrecedence() {
         assertThat(testResult(WithExplicitGenerator.class), isSuccessful());
         assertEquals(asList(0, 1, 2, 3, 4), WithExplicitGenerator.values);
+        WithExplicitGenerator.values.clear();
     }
 
     @RunWith(JUnitQuickcheck.class)
