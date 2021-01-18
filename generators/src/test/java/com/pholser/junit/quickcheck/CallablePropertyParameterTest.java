a905910d7cb7947fc103018d915bcbd60c47753f
Zhengxi Li <lzx404243@hotmail.com>
2021-01-17 18:10:33 -0600

    Reset CallableOfInt.iterations to 0 after test callable()

--- generators/src/test/java/com/pholser/junit/quickcheck/CallablePropertyParameterTest.java
+++ generators/src/test/java/com/pholser/junit/quickcheck/CallablePropertyParameterTest.java
@@ -43,6 +43,7 @@ public class CallablePropertyParameterTest {
     @Test public void callable() throws Exception {
         assertThat(testResult(CallableOfInt.class), isSuccessful());
         assertEquals(defaultPropertyTrialCount(), CallableOfInt.iterations);
+        CallableOfInt.iterations = 0;
     }
 
     @RunWith(JUnitQuickcheck.class)
